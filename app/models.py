1426f6a1d694682e3b203494d2836d0f5c0ba0cb
Apua <Apua.A.Aa@gmail.com>
2017-10-12 21:24:17 +0800

    update REST API with Python CouchDB

--- app/models.py
+++ app/models.py
@@ -1,53 +1,68 @@
-class TestData:
-    r"""A workaround version input/output test data "model" with plain text.
+if __name__=='__main__':
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-    A test data includes:
+import couchdb
+from couchdb.mapping import Document, TextField, IntegerField, DateTimeField
 
-    - test library
-    - test suite
-      - test case
-      - variables
-      - keywords
-    - resource and variable files (for sharing)
+from app.utils import _v, _t
 
-    It is recursive in JSON, eg:
 
-    >>> test_data = TestData({
-    ...     'top_level': {  # suite folder name
-    ...         '__init__.robot': '***settings***\nresource resource.robot\nsuite setup  init\n',
-    ...         '2__suite.robot': '***test cases***\n1st Case\n    log_to_console  suite 2\n',
-    ...         '1__suite.robot': '***test cases***\n1st Case\n    log_to_console  suite 1\n',
-    ...         'resource.robot': '***keywords***\ninit\n    log_to_console  init lol\n',
-    ...         }
-    ...     })
-    """
-    instances = {}
-    last_id = 0
+couch = couchdb.Server('http://apua:qwer1234@localhost:9453')
+db = couch['poc'] if 'poc' in couch else couch.create('poc')
 
+
+class DocumentManager:
+    def __init__(self, db):
+        self.db = db
+
+    def __get__(self, instance, owner):
+        self.owner = owner
+        return self
+
+    def delete(self, id):
+        db.delete(self.get(id))
+
+    def get(self, id):
+        return self.owner.load(self.db, id)
+
+    def all(self):
+        """
+        return documents, ref:
+
+        - https://pythonhosted.org/CouchDB/client.html#database
+        - https://docs.djangoproject.com/en/1.11/topics/db/queries/#retrieving-all-objects
+        """
+        # TODO: It is about Python CouchDB API, `db.values()` is not implemented.
+        return [self.get(id) for id in self.db]
+
+
+class TestData(Document):
+    data = TextField()
+    objects = DocumentManager(db)
     example = '***test cases***\n1st Case\n    log_to_console  suite 1\n'
 
-    def __init__(self, test_data):
-        self.data = test_data
+    def save(self):
+        _v('db')
+        self.store(db)
 
-    def __del__(self):
-        __class__.instances[self.id] = None
+    def delete(self):
+        del db[self.id]
 
-    def to_dict(self):
-        return {'data': self.data}
+    @classmethod
+    def post_schema(cls, packed):
+        return cls(**packed)
 
-    @staticmethod
-    def add(testdata):
-        __class__.last_id += 1
-        __class__.instances[__class__.last_id] = testdata
-        testdata.id = __class__.last_id
-        return testdata.id
+    def to_json(self):
+        return {'id': self.id, **self._data}
 
-    def run(self):
-        create_temporary_files()
-        console = run_test_suite_via_subprocess()
-        report, log, output = read_test_result()
-        return {'report': report, 'log': log, 'output': output, 'console': console}
+    def update(self, d):
+        _v('d')
+        return self._data.update(d)
+
+    # def run(self): "run job by another model
 
 
-class ID(int):
-    """Nothing but ID"""
+class Id(str):
+    def __new__(cls, uuid: __import__('uuid').UUID):
+        return super().__new__(cls, uuid.hex)
--- app/views.py
+++ app/views.py
@@ -1,55 +1,63 @@
-from hug import get, post, put, delete, http
 import hug
+from hug import get, post, put, delete, http
+from hug.output_format import html as html_format
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-from .models import TestData, ID
+from app.models import TestData, Id
+from app.utils import _v, _t
 
+# TODO: Clean `render` arguments to avoid `locals` and `globals`.
 env = Environment(
         loader=PackageLoader('app', 'templates'),
         autoescape=select_autoescape(['html', 'xml']),
         )
 
 
-@get('/', output=hug.output_format.html)
+@get('/', output=html_format)
 def _():
     return env.get_template('list.html').render(**locals(), **globals())
 
 
-@get('/{id:int}/detail', output=hug.output_format.html)
-def _(id: ID):
+@get('/{id:uuid}/detail', output=html_format)
+def _(id: Id):
     return env.get_template('detail.html').render(id=id, detail=get_testdata(id)['data'])
 
 
-@get('/add', output=hug.output_format.html)
+@get('/add', output=html_format)
 def _():
     return env.get_template('add.html').render(**locals(), **globals())
 
 
 @post('/')
-def _(request, response, body: TestData):
-    new_id = TestData.add(body)
+def _(body: TestData.post_schema, request, response):
+    td = body
+    td.save()
     if request.user_agent and 'Mozilla' in request.user_agent:  # browser
         response.status = hug.HTTP_303
-        response.location = f'/{new_id}/detail'
+        response.location = f'/{td.id}/detail'
     else:  # o.w.
         response.status = hug.HTTP_201
-        response.location = f'/{new_id}'
+        response.location = f'/{td.id}'
 
 
-@get('/{id:int}')
-def get_testdata(id: ID):
-    testdata = TestData.instances[id]
-    return testdata.to_dict()
+@get('/{id:uuid}')
+def get_testdata(id: Id):
+    # TODO: Error handler with HTTP status code (eg: 404)
+    return TestData.objects.get(id).to_json()
 
 
-@put('/{id:int}')
-def _(id: ID, body: TestData):
-    TestData.instances[id] = body
+@put('/{id:uuid}')
+def _(id: Id, body: TestData.post_schema):
+    # TODO: Error handler with HTTP status code (eg: 404)
+    td = TestData.objects.get(id)
+    # TODO: It is about Python CouchDB API, `dict(body)` will fail, thus
+    #       cannot simply code `td.update(body)`.
+    td.update(body.items())
+    td.save()
 
 
-@delete('/{id:int}')
-def _(id: ID):
-    del TestData.instances[id]
-
-
+@delete('/{id:uuid}')
+def _(id: Id):
+    # TODO: Error handler with HTTP status code (eg: 404)
+    TestData.objects.delete(id)
