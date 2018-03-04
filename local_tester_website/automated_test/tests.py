8bcf204cb96f6ce3c62abbc20091808268e69ae4
Apua <Apua.A.Aa@gmail.com>
2018-03-05 02:01:50 +0800

    refactor: make "adding for doctest" pluggable

--- local_tester_website/automated_test/tests.py
+++ local_tester_website/automated_test/tests.py
@@ -2,9 +2,11 @@ import doctest
 from django.test import TestCase
 
 def load_tests(loader, tests, ignore):
-    from importlib import import_module
-    for mod_name in ('tasks', 'models'):
-        tests.addTests(doctest.DocTestSuite(
-            import_module(f'.{mod_name}', __package__)
-            ))
+    def doctest_it(mod_name):
+        from importlib import import_module
+        mod = import_module(f'.{mod_name}', __package__)
+        tests.addTests(doctest.DocTestSuite(mod))
+
+    doctest_it('tasks')
+    doctest_it('models')
     return tests
