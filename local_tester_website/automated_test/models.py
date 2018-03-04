412962dbb846e951696f5076d6900e4f6a70e9db
Apua <Apua.A.Aa@gmail.com>
2018-03-04 22:49:17 +0800

    poc: make `models.py` doctest be context-free

--- local_tester_website/automated_test/models.py
+++ local_tester_website/automated_test/models.py
@@ -3,8 +3,7 @@ r"""
 Create instance and save to database in one line::
 
     >>> #TestExecution().save()
-    >>> TestExecution.objects.create()
-    <TestExecution: TestExecution object (1)>
+    >>> te = TestExecution.objects.create()
 
 Get by `id` field or `pk` argument::
 
@@ -30,11 +29,11 @@ Note that don't insert bytes string into `output` char field::
     >>> pb1 = Pybot.objects.create(pid=9487, output=line)
     >>> pb1.output
     b'3 critical tests, 3 passed, 0 failed'
-    >>> pb2 = Pybot.objects.get(pk=1)
+    >>> pb2 = Pybot.objects.get(pk=pb1.id)
     >>> pb2.output
     "b'3 critical tests, 3 passed, 0 failed'"
     >>> pb3 = Pybot.objects.create(pid=9487, output=line.decode())
-    >>> pb3 = Pybot.objects.get(pk=2)
+    >>> pb3 = Pybot.objects.get(pk=pb3.id)
     >>> pb3.output
     '3 critical tests, 3 passed, 0 failed'
 """
