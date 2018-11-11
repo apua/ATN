ea8b7c9966b476a3953983d77dff41cf54b1817a
Apua <Apua.A.Aa@gmail.com>
2018-11-11 16:09:37 +0800

    Add URI design

--- cirrus/autotest/views.py
+++ cirrus/autotest/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from django.http import JsonResponse  #, HttpResponse, StreamingHttpResponse
+
+
+class SuiteIndexView(View):
+    """
+    get list, get query some, post new
+    """
+    def get(self, request):
+        return JsonResponse({})
+
+    def post(self, request):
+        return JsonResponse({})
+
+
+class SuiteDetailView(View):
+    """
+    get detail, delete, put to edit
+    """
+    def get(self, request):
+        return JsonResponse({})
+
+    def put(self, request):
+        return JsonResponse({})
+
+    def delete(self, request):
+        return JsonResponse({})
+
+
+class TaskIndexView(View):
+    """
+    get list, get query some, post to execute suite with suts
+    """
+    def get(self, request):
+        return JsonResponse({})
+
+    def post(self, request):
+        return JsonResponse({})
+
+
+class TaskDetailView(View):
+    """
+    get state and report, delete, post to re-execute its test data and variables
+    """
+    def get(self, request):
+        return JsonResponse({})
+
+    def post(self, request):
+        return JsonResponse({})
+
+    def delete(self, request):
+        return JsonResponse({})
